308669c657e1105a036a73b9517c9641e3bd5834
Michael Kriese <michael.kriese@visualon.de>
2019-03-21 09:58:28 +0100

    build: remove bower

--- grunt-cfg/copy.js
+++ grunt-cfg/copy.js
@@ -1,42 +1,35 @@
 var os = require('os');
 
-module.exports = function (grunt, options) {
+module.exports = function(grunt, options) {
     return {
-        "bin": {
-            "files": [
-            {
-                "flatten": true,
-                "expand": true,
-                "cwd": "bower_components/metro/fonts/",
-                "src": "*.*",
-                "dest": "<%= path.fonts %>"
-            },
-            {
-                "flatten": true,
-                "expand": true,
-                "cwd": "bower_components/font-awesome/fonts/",
-                "src": "*.*",
-                "dest": "<%= path.fonts %>"
-            },
-            {
-                "expand": true,
-                "flatten": true,
-                "src": [
-                    "bower_components/jquery/dist/jquery.js",
-                    "bower_components/metro/build/js/metro.js",
-                    "bower_components/waypoints/lib/jquery.waypoints.js",
-                    "bower_components/cookieconsent2/cookieconsent.js"
-                ],
-                "dest": "<%= path.js %>"
-            },
-            {
-                "flatten": true,
-                "src": [
-                    "doc/*.gitignore",
-                ],
-                "dest": "<%= path.bin %>.gitignore"
-            }
-            ]
-        }
-    }
+        bin: {
+            files: [
+                {
+                    flatten: true,
+                    expand: true,
+                    cwd: 'node_modules/metro-ui/build/fonts/',
+                    src: '*.*',
+                    dest: '<%= path.fonts %>',
+                },
+                {
+                    flatten: true,
+                    expand: true,
+                    cwd: 'node_modules/font-awesome/fonts/',
+                    src: '*.*',
+                    dest: '<%= path.fonts %>',
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/jquery/dist/jquery.js', 'node_modules/metro-ui/build/js/metro.js', 'node_modules/waypoints/lib/jquery.waypoints.js'],
+                    dest: '<%= path.js %>',
+                },
+                {
+                    flatten: true,
+                    src: ['doc/*.gitignore'],
+                    dest: '<%= path.bin %>.gitignore',
+                },
+            ],
+        },
+    };
 };

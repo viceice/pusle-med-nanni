c7214967d8410ca5db194eaf8e7f3761c1e13133
Michael Kriese <michael.kriese@gmx.de>
2024-01-11 13:11:02 +0100

    ci: debug output

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -2,21 +2,20 @@
 
 set -eo pipefail
 
+echo starting ssh-agent
 eval "$(ssh-agent -s)"
 
+echo exporting ssh-agent variables
 echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> "$GITHUB_ENV"
 echo "SSH_AGENT_PID=$SSH_AGENT_PID" >> "$GITHUB_ENV"
-
-echo "generating key"
-ssh-keygen -t ed25519 -C "test" -f ~/.ssh/id_ed25519 -q -N ""
-echo "loading generated key"
-ssh-add - <  ~/.ssh/id_ed25519
 
 echo "loading encrypted key"
 ssh-add - <(sops -d .github/secrets/ssh-pk.enc.pem)
 
+echo preparing known_hosts
 echo "vps577063.ovh.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuuDnpHPBfoW7jZjEqTVrbkQXdG42sAX4bDscc90utS" >> ~/.ssh/known_hosts
 
-ssh gitlab@vps577063.ovh.net ps
+echo testing connection
+ssh gitlab@vps577063.ovh.net id
 
 exit 1

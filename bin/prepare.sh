8ee5fc7d30fa19ad281ef18b30a48ee03ac2f3b9
Michael Kriese <michael.kriese@gmx.de>
2024-01-11 13:12:04 +0100

    ci: fix ssh key loading

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -10,7 +10,7 @@ echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> "$GITHUB_ENV"
 echo "SSH_AGENT_PID=$SSH_AGENT_PID" >> "$GITHUB_ENV"
 
 echo "loading encrypted key"
-ssh-add - <(sops -d .github/secrets/ssh-pk.enc.pem)
+sops -d .github/secrets/ssh-pk.enc.pem | ssh-add -
 
 echo preparing known_hosts
 echo "vps577063.ovh.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuuDnpHPBfoW7jZjEqTVrbkQXdG42sAX4bDscc90utS" >> ~/.ssh/known_hosts

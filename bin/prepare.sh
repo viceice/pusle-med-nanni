3d9b608bb291d78ea1fd7e3c4bacbff097586cbf
Michael Kriese <michael.kriese@gmx.de>
2024-01-11 12:14:31 +0100

    ci: use sops

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -7,6 +7,6 @@ eval "$(ssh-agent -s)"
 echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> "$GITHUB_ENV"
 echo "SSH_AGENT_PID=$SSH_AGENT_PID" >> "$GITHUB_ENV"
 
-ssh-add - <<< "$SSH_PRIVATE_KEY"
+sops -d .github/secrets/ssh-pk.enc.pem | ssh-add -
 
 echo "vps577063.ovh.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuuDnpHPBfoW7jZjEqTVrbkQXdG42sAX4bDscc90utS" >> ~/.ssh/known_hosts
